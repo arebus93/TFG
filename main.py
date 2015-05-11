8aab9a2b9d2dc16ae95f2c031e201c5e88840d19
Adrian Arevalo <adriestu@hotmail.com>
2015-05-11 14:18:05 +0000

    Dision en modulos, version preeliminar base de datos

--- main.py
+++ main.py
@@ -1,34 +1,7 @@
 #!/usr/bin/python
 
-#import database
-import serial
-import RPi.GPIO as GPIO
-import time
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(17,GPIO.OUT)
-GPIO.output(17,GPIO.HIGH) 
-time.sleep(0.5)
-
-def leerSerial(): 
- print "Inicio de lectura puerto serie"
- while True:
-   if s.inWaiting():
-    print  s.readline()
-
-def comandosAT():
- GPIO.output(17,GPIO.LOW)
- time.sleep(0.5)
- print "Inicio de comandos AT" 
- while True:
-  comando=raw_input()
-  s.write(comando)
-  print s.readline()
-  print s.readline() #Descomentar para comandos
-  print s.readline() #con mas valores de retorno 
-  print s.readline()
-  print s.readline()
+import database as db
+import serie as ser
 
 #------------------------------------------
 #   Programa Principal
@@ -37,21 +10,12 @@ Puerto= '/dev/ttyAMA0'
 Vel=9600
 Timeout=1
 
-try:
-	s=serial.Serial(Puerto,Vel)
-	s.timeout=Timeout
-	s.open()
+db.crearTablas()
+db.nuevoSensor(1001,'Temperatura', 1)
+db.mostrarSensores()
 
-except serial.SerialException:
-   #-- Error al abrir el puerto serie
-   sys.stderr.write("Error al abrir puerto (%s)\n" % str(Puerto))
-   sys.exit(1)
-
-   #-- Mostrar el nombre del dispositivo serie utilizado
-print "Puerto: %s" %(Puerto)
-
-#comandosAT() #--Configuracion del modulo HC-11 con comandos AT
-leerSerial()
-s.close() #-- Cerramos el puerto serie
-print "Puerto: %s cerrado" %(Puerto)
+#ser.initSerial(Puerto,Vel,Timeout)
+#ser.comandosAT()
+#ser.leerSerial()
+#ser.closeSerial()
 
